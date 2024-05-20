d99f27c18b30e607df891a157d0e8309242e662d
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2024-05-20 10:31:22 +0200

    changes

--- task2.py
+++ task2.py
@@ -2,28 +2,46 @@ import turtle
 import math
 
 def draw_pythagoras_tree(length, level):
+    """
+    Функція для малювання фрактала "дерево Піфагора" за допомогою рекурсії.
+
+    Параметри:
+    - length: довжина початкової гілки
+    - level: рівень рекурсії, глибина дерева
+    """
+
+    # Базовий випадок для рекурсії: якщо рівень 0, повернення
     if level == 0:
         return
     
+    # Малюємо початкову гілку
     turtle.forward(length)
 
+    # Поворот та рекурсивний виклик для лівої гілки
     turtle.left(45)
-    draw_pythagoras_tree(length *(math.sqrt(2) / 2), level - 1) 
+    draw_pythagoras_tree(length * (math.sqrt(2) / 2), level - 1) 
 
+    # Поворот та рекурсивний виклик для правої гілки
     turtle.right(90)
-    draw_pythagoras_tree(length *(math.sqrt(2) / 2), level - 1)
+    draw_pythagoras_tree(length * (math.sqrt(2) / 2), level - 1)
 
+    # Повернення до початкового положення
     turtle.left(45)
     turtle.backward(length)
 
 def main():
-    level = int(input("Enter the level of the tree: "))
+    # Запит користувача на введення рівня рекурсії
+    level = int(input("Enter the level of recursion: "))
 
+    # Налаштування швидкості малювання та орієнтація turtle
     turtle.speed("fastest")
     turtle.left(90)
+    turtle.color("green")
 
+    # Виклик функції для малювання дерева
     draw_pythagoras_tree(100, level)
 
+    # Виклик функції для завершення turtle
     turtle.done()
 
 if __name__ == "__main__":
